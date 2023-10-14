f521de4f4d7ce7c182b5e98ac2a61800fb956e7e
Omar Thabet <mr.omar.atef@hotmail.com>
2023-10-15 00:45:25 +0300

    Adding more test cases

--- shredder_test.go
+++ shredder_test.go
@@ -46,3 +46,42 @@ func TestFile(t *testing.T) {
         t.Fatalf("File test failed: %v", err)
     }
 }
+
+func TestShredNonExistentFile(t *testing.T) {
+    err := Shred("/nonexistent/file")
+    if err == nil {
+        t.Error("Expected an error when shredding a non-existent file, but got none.")
+    }
+}
+
+func TestShredEmptyFile(t *testing.T) {
+    // Create a temporary empty file for testing
+    tempFile, err := ioutil.TempFile("", "empty_file_test")
+    if err != nil {
+        t.Fatalf("Failed to create temporary file: %v", err)
+    }
+    defer tempFile.Close()
+    defer os.Remove(tempFile.Name())
+
+    // Test the Shred function on an empty file
+    if err := Shred(tempFile.Name()); err != nil {
+        t.Fatalf("Shred test on an empty file failed: %v", err)
+    }
+}
+
+func TestFileRemoveTrue(t *testing.T) {
+    // Test with Remove set to true
+    config := Config{Iterations: 3, Remove: true}
+    // ... create a temporary file and write data as before
+    if err := config.File(tempFile.Name()); err != nil {
+        t.Fatalf("File test with Remove=true failed: %v", err)
+    }
+}
+
+func TestFileNonExistentFile(t *testing.T) {
+    config := Config{Iterations: 3, Remove: true}
+    err := config.File("/nonexistent/file")
+    if err == nil {
+        t.Error("Expected an error when working with a non-existent file, but got none.")
+    }
+}
