df7c9808712e769b4bd4f8acdd78cbe2b69b7877
Omar Thabet <mr.omar.atef@hotmail.com>
2023-10-14 17:17:01 +0300

    modify testing

--- shredder_test.go
+++ shredder_test.go
@@ -1,22 +1,52 @@
 package shredder
 
 import (
-	"fmt"
-    "os"
     "testing"
+    "os"
+    "io/ioutil"
 )
 
 func TestShred(t *testing.T) {
-	
-	if len(os.Args) != 2 {
-        os.Exit(1)
-    }
-    Input := Config{iterations: 3, remove: false}
-    Path := os.Args[1]
-    err := Input.File(Path)
+    // Create a temporary file for testing
+    tempFile, err := ioutil.TempFile("", "shred_test")
     if err != nil {
-        t.Fatalf("Error: %v", err)
-    } else {
-        fmt.Println("File shredded")
+        t.Fatalf("Failed to create temporary file: %v", err)
     }
+    defer tempFile.Close()
+    defer os.Remove(tempFile.Name())
+
+    // Write some data to the temporary file
+    testData := []byte("This is some test data.")
+    if _, err := tempFile.Write(testData); err != nil {
+        t.Fatalf("Failed to write data to temporary file: %v", err)
+    }
+
+    // Test the Shred function
+    if err := Shred(tempFile.Name()); err != nil {
+        t.Fatalf("Shred test failed: %v", err)
+    }
+    // You can add assertions to verify the file was properly shredded here.
+}
+
+func TestFile(t *testing.T) {
+    // Create a temporary file for testing
+    tempFile, err := ioutil.TempFile("", "file_test")
+    if err != nil {
+        t.Fatalf("Failed to create temporary file: %v", err)
+    }
+    defer tempFile.Close()
+    defer os.Remove(tempFile.Name())
+
+    // Write some data to the temporary file
+    testData := []byte("This is some test data.")
+    if _, err := tempFile.Write(testData); err != nil {
+        t.Fatalf("Failed to write data to temporary file: %v", err)
+    }
+
+    // Test the File function
+    config := Config{Iterations: 3, Remove: true}
+    if err := config.File(tempFile.Name()); err != nil {
+        t.Fatalf("File test failed: %v", err)
+    }
+    // You can add assertions to verify the file was properly shredded and removed here.
 }
